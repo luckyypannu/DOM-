aeff3ce306dd913b0bc88dda0ee05b6d3e89295a
luckyypannu <luckypannu2005@gmail.com>
2026-01-29 11:52:43 +0530

    callback function

--- dom.js
+++ dom.js
@@ -1,24 +1,24 @@
-const para = document.getElementsByClassName("para")
-para[0].innerText = "this is a new paragraph"
-para[1].innerText = "hi guys!!"
-// para[0].style.color = "green"
-// para[1].style.color = "red"
-console.log(para)
-console.log("Hello")
+// const para = document.getElementsByClassName("para")
+// para[0].innerText = "this is a new paragraph"
+// para[1].innerText = "hi guys!!"
+// // para[0].style.color = "green"
+// // para[1].style.color = "red"
+// console.log(para)
+// console.log("Hello")
 
-for(let i=0;i<para.length;i++){
-    para[i].style.color = "green"
-}
+// for(let i=0;i<para.length;i++){
+//     para[i].style.color = "green"
+// }
 
 // const abc = document.querySelector(".abc")
 // abc.innerText = "1234"
 
-const abc = document.querySelectorAll(".abc")
-abc[0].innerText = "12345"
-abc[1].innerText = "1213456"
+// const abc = document.querySelectorAll(".abc")
+// abc[0].innerText = "12345"
+// abc[1].innerText = "1213456"
 
-const container = document.querySelector(".container")
-container.innerHTML = "<h1>dom class</h1>"
+// const container = document.querySelector(".container")
+// container.innerHTML = "<h1>dom class</h1>"
 
 // function message(event)
 // {
@@ -57,35 +57,84 @@ container.innerHTML = "<h1>dom class</h1>"
 
 // print()
 
-function first(){
-    second()
-}
-
-function second(){
-    third()
-}
-
-function third(){
-    console.trace()
-}
-
-first()
-
-// function infinite(){
-//     infinite()
+// function first(){
+//     second()
 // }
 
-// infinite()
+// function second(){
+//     third()
+// }
 
-let total = 40
+// function third(){
+//     console.trace()
+// }
 
-function calculate(){
-    console.log(total)
-    let total = 100
+// first()
+
+// // function infinite(){
+// //     infinite()
+// // }
+
+// // infinite()
+
+// let total = 40
+
+// function calculate(){
+//     console.log(total)
+//     let total = 100
+// }
+
+// calculate()
+
+// console.log("first line")
+// setTimeout(()=>{console.log("after 2 sec")},2000)
+// console.log("2nd line")
+
+// setTimeout(()=>{
+//     alert("line after 3 sec")
+// }, 2*1000)
+
+// setInterval(()=>{9
+//     console.log("setInterval")
+// },1000)
+
+// let num = 1
+
+// const id = setInterval(()=>{
+//     if (num==10)clearInterval(id)
+//     console.log(num)
+//     num+=1
+// },1000)
+    
+// const name=document.querySelector("#name")
+// const btn=document.querySelector(".btn")
+// const list=document.querySelector(".list")
+// btn.addEventListener('click',()=>{
+//     if(name.value===""){
+//         alert("enter the name first")
+//         return;
+//     }
+//     const li=document.createElement('li' )
+//     const dlt=document.createElement('button')
+//     dlt.innerText="delete"
+//     li.innerText=name.value;
+//     dlt.addEventListener("click",()=>{
+//         list.removeChild(li)
+//     })
+//     li.appendChild(dlt)
+//     list.appendChild(li)
+//     name.value=""
+// })
+
+function print(num){
+    setTimeout(() => {
+        console.log("inside print")
+         num() 
+    }, 2000);
 }
 
-calculate()
+function sample(){
+    console.log("inside callback")
+}
 
-    
-
-    
+print(sample)
